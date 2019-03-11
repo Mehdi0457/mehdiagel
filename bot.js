5cffba8899dacb056b8e34ac8527d201ac321fe1
Mehdi0457 <48065343+Mehdi0457@users.noreply.github.com>
2019-03-11 11:07:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,85 +1,65 @@
-const var prefix = "D"
-client.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== '226442200') return;
-
-if (message.content.startsWith(prefix + 'p')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(  {argresult})
-} else 
-
-if (message.content.startsWith(prefix + 'w')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(  {argresult})
-} else 
-if (message.content.startsWith(prefix + 'l')) {
-client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(  {argresult})
-} else 
-
-if (message.content.startsWith(prefix + 's')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
-    message.channel.sendMessage(  {argresult})
-}
-
+if(message.content.startsWith(prefix + "setlistening")){
+let hargs = message.content.split(' ').slice(1).join(' ');
+if(!hargs) return message.reply("**الرجاء وضع الكلام الذي تريده**");
+if(hargs.length < 1 || hargs.length > 50) return message.reply("مع الأسف اخوي ... م ينفع يكون اكثر من 50 حرف");
+message.delete();
+bot.user.setActivity(hargs, {
+type: "LISTENING"
 });
-
-const prefix = "D"
-client.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== '226442200') return;
-
-if (message.content.startsWith(prefix + 'p')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(  : ${argresult})
-} else 
-
-if (message.content.startsWith(prefix + 'w')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(  : ${argresult})
-} else 
-if (message.content.startsWith(prefix + 'l')) {
-client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(  : ${argresult})
-} else 
-
-if (message.content.startsWith(prefix + 's')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
-    message.channel.sendMessage(  : ${argresult})
+message.channel.send(`**Done! it's now Listening To:** ${hargs}`)
 }
-
+ 
+if(message.content.startsWith(prefix + "setwatching")){
+let hargs = message.content.split(' ').slice(1).join(' ');
+if(!hargs) return message.reply("**الرجاء وضع الكلام الذي تريده**");
+if(hargs.length < 1 || hargs.length > 50) return message.reply("**مع الأسف  ... م ينفع يكون اكثر من 50 حرف**");
+message.delete();
+bot.user.setActivity(hargs, {
+type: "WATCHING"
 });
-
-const prefix = "D"
-client.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== '226442200') return;
-
-if (message.content.startsWith(prefix + 'p')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(  : ${argresult})
-} else 
-
-if (message.content.startsWith(prefix + 'w')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(  : ${argresult})
-} else 
-if (message.content.startsWith(prefix + 'l')) {
-client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(  : ${argresult})
-} else 
-
-if (message.content.startsWith(prefix + 's')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
-    message.channel.sendMessage(  : ${argresult})
+message.channel.send(`**Done! it's now Watching:** (${hargs})`)
 }
-
+ 
+if(message.content.startsWith(prefix + "setstreaming")){
+ let hargs = message.content.split(' ').slice(1).join(' ');
+if(!hargs) return message.reply("**الرجاء وضع الكلام الذي تريده**");
+if(hargs.length < 1 || hargs.length > 50) return message.reply("**مع الأسف  ... م ينفع يكون اكثر من 50 حرف**");
+message.delete();
+bot.user.setActivity(hargs, {
+type: "STREAMING",
+             url: "https://www.twitch.tv/AzoqzMj"
 });
+message.channel.send(`**Done! It's now Streaming:** ${hargs}`)
+}
+ 
+if(message.content.startsWith(prefix + "setplaying")){
+let hargs = message.content.split(' ').slice(1).join(' ');
+if(!hargs) return message.reply("**الرجاء وضع الكلام الذي تريده**");
+if(hargs.length < 1 || hargs.length > 50) return message.reply("**مع الأسف  ... م ينفع يكون اكثر من 50 حرف**");
+message.delete();
+bot.user.setActivity(hargs, {
+type: "PLAYING"
+});
+message.channel.send(`**Done! It's now Streaming:** ${hargs}`)
+}
+if(message.content.startsWith(prefix + "setname")){
+let hargs = message.content.split(' ').slice(1).join(' ');
+if(!hargs) return message.reply("**الرجاء كتابة اسم البوت الذي تريده**");
+if(hargs.length < 1 || hargs.length > 32) return message.reply("**مع الأسف  ... م ينفع يكون اكثر من 32 حرف**");
+message.delete();
+bot.user.setUsername(hargs)
+message.channel.send(`**Done! My New Name is:** ${hargs}`)
+}
+if(message.content.startsWith(prefix + "setavatar")){
+let hargs = message.content.split(' ').slice(1).join(' ');
+if(!hargs) return message.reply("**الرجاء وضع رابط الصورة**");
+message.delete();
+bot.user.setAvatar(hargs)
+message.channel.send(`**Done! The new picture is:** ${hargs}`)
+}
+if(message.content.startsWith(prefix + "setnothing")){
+ bot.user.setActivity("")
+ message.channel.send(`Done!`)
+ }
 
 client.login(process.env.BOT_TOKEN);
