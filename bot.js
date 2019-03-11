a20650cdf0da8eeb063f04574d2a7edaa8f40798
Mehdi0457 <48065343+Mehdi0457@users.noreply.github.com>
2019-03-11 10:56:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,21 +7,21 @@ client.on('message', message => {
 
 if (message.content.startsWith(prefix + 'p')) {
   client.user.setGame(argresult);
-    message.channel.sendMessage(   ${argresult})
+    message.channel.sendMessage(  {argresult})
 } else 
 
 if (message.content.startsWith(prefix + 'w')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(   ${argresult})
+    message.channel.sendMessage(  {argresult})
 } else 
 if (message.content.startsWith(prefix + 'l')) {
 client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(   ${argresult})
+    message.channel.sendMessage(  {argresult})
 } else 
 
 if (message.content.startsWith(prefix + 's')) {
   client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
-    message.channel.sendMessage(   ${argresult})
+    message.channel.sendMessage(  {argresult})
 }
 
 });
@@ -59,7 +59,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== '505072087287726090') return;
+  if (message.author.id !== '226442200') return;
 
 if (message.content.startsWith(prefix + 'p')) {
   client.user.setGame(argresult);
